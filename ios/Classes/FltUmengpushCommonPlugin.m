3629eab1983209831341ab5a65b135d23f75774c
RandyWei <smile561607154@163.com>
2019-11-01 17:39:00 +0800

    ios upush common

--- ios/Classes/FltUmengpushCommonPlugin.m
+++ ios/Classes/FltUmengpushCommonPlugin.m
@@ -1,17 +1,42 @@
 #import "FltUmengpushCommonPlugin.h"
+#import <UIKit/UIKit.h>
+#import <UMCommon/UMCommon.h>
 
 @implementation FltUmengpushCommonPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flt_umengpush_common"
+      methodChannelWithName:@"plugin.bughub.dev/flt_umengpush_common"
             binaryMessenger:[registrar messenger]];
   FltUmengpushCommonPlugin* instance = [[FltUmengpushCommonPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"init" isEqualToString:call.method]) {
+    
+      NSString* appKey = call.arguments[@"appKey"];
+      NSString* channel = call.arguments[@"channel"];
+      
+      if (appKey==nil||[appKey isEqual:[NSNull null]]||[appKey isEqualToString:@""]) {
+          result([FlutterError errorWithCode:@"Error" message:@"appKey is null" details:nil]);
+          return;
+      }
+      
+      if (channel==nil||[channel isEqual:[NSNull null]]||[channel isEqualToString:@""]) {
+          result([FlutterError errorWithCode:@"Error" message:@"channel is null" details:nil]);
+          return;
+      }
+      
+      [UMConfigure initWithAppkey:appKey channel:channel];
+      
+      result(nil);
+  } else if ([@"setLogEnabled" isEqualToString:call.method]) {
+    
+      BOOL bFlag = [call.arguments[@"enabled"] boolValue];
+      
+      [UMConfigure setLogEnabled:bFlag];
+      
+      result(nil);
   } else {
     result(FlutterMethodNotImplemented);
   }
--- ios/flt_umengpush_common.podspec
+++ ios/flt_umengpush_common.podspec
@@ -3,18 +3,19 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'flt_umengpush_common'
-  s.version          = '0.0.1'
+  s.version          = '1.0.0'
   s.summary          = 'A new Flutter plugin by umeng push common'
   s.description      = <<-DESC
 A new Flutter plugin by umeng push common
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'https://www.bughub.dev/'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Your Company' => 'smile561607154@163.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency 'UMCCommon'
 
   s.ios.deployment_target = '8.0'
 end
