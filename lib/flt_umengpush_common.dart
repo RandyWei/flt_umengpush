061eb52f8222db14aa782abf9139c4fc7827d88f
RandyWei <smile561607154@163.com>
2019-11-04 09:03:44 +0800

    添加注释

--- lib/flt_umengpush_common.dart
+++ lib/flt_umengpush_common.dart
@@ -4,17 +4,25 @@ import 'dart:ffi';
 import 'package:flutter/services.dart';
 
 class FltUmengpushCommon {
+  /*设备类型：手机 */
   static const int DEVICE_TYPE_PHONE = 1;
+  /*设备类型：盒子 */
   static const int DEVICE_TYPE_BOX = 2;
 
   static const MethodChannel _channel =
       const MethodChannel('plugin.bughub.dev/flt_umengpush_common');
 
+  /*
+   * 初始化方法
+   */
   static Future<Void> init(String appKey, String secret, [String channel, int deviceType]) async {
     return await _channel.invokeMethod(
         'init', {"appKey": appKey, "secret": secret, "channel": channel, "deviceType": deviceType});
   }
 
+  /*
+  * 是否开启日志
+  */
   static Future<Void> setLogEnabled(bool enabled) async {
     return await _channel.invokeMethod('setLogEnabled', {"enabled": enabled});
   }
