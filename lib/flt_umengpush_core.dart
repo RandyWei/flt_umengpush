6cfcc0a96b0073bbcb3ba5ab6618a9993e48b079
RandyWei <smile561607154@163.com>
2019-11-11 16:38:40 +0800

    Migrate to AndroidX

--- lib/flt_umengpush_core.dart
+++ lib/flt_umengpush_core.dart
@@ -7,14 +7,17 @@ typedef TokenCallBack(String token);
 typedef NotificationCallback(dynamic pushData);
 
 class FltUmengpushCore {
-  static const MethodChannel _channel = const MethodChannel('plugin.bughub.dev/flt_umengpush_core');
+  static const MethodChannel _channel =
+      const MethodChannel('plugin.bughub.dev/flt_umengpush_core');
 
   /*
   * 监听通知
   * */
   static Future<Void> listen(
-      {TokenCallBack tokenCallback, NotificationCallback notificationCallback}) async {
-    EventChannel eventChannel = const EventChannel("plugin.bughub.dev/flt_umengpush_core/event");
+      {TokenCallBack tokenCallback,
+      NotificationCallback notificationCallback}) async {
+    EventChannel eventChannel =
+        const EventChannel("plugin.bughub.dev/flt_umengpush_core/event");
     eventChannel.receiveBroadcastStream().listen((channelData) {
       var event = channelData["event"];
       if (event == "configure") {
