a434bd9afd1a64728d57d5d7a765e72c9e68f9b0
weizhengheng <weizhengheng@chinahrt.com>
2021-11-26 15:06:35 +0800

    plugin v2

--- flt_umengpush_core/android/src/main/kotlin/dev/bughub/plugin/flt_umengpush_core/FltUmengpushCorePlugin.kt
+++ flt_umengpush_core/android/src/main/kotlin/dev/bughub/plugin/flt_umengpush_core/FltUmengpushCorePlugin.kt
@@ -1,5 +1,6 @@
 package dev.bughub.plugin.flt_umengpush_core
 
+import android.app.Activity
 import android.content.Context
 import android.util.Log
 import com.umeng.message.IUmengRegisterCallback
@@ -7,6 +8,9 @@ import com.umeng.message.PushAgent
 import com.umeng.message.UTrack
 import com.umeng.message.common.inter.ITagManager
 import com.umeng.message.tag.TagManager
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -14,86 +18,23 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FltUmengpushCorePlugin(var registrar: Registrar) : MethodCallHandler {
+class FltUmengpushCorePlugin : MethodCallHandler, FlutterPlugin, ActivityAware {
 
-    val eventSink = QueuingEventSink()
+    private val eventSink = QueuingEventSink()
 
-
-    companion object {
-
-        @JvmField
-        var corePlugin: FltUmengpushCorePlugin? = null
-
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-
-            corePlugin = FltUmengpushCorePlugin(registrar)
-
-            val channel = MethodChannel(registrar.messenger(), "plugin.bughub.dev/flt_umengpush_core")
-            channel.setMethodCallHandler(corePlugin)
-            val eventChannel = EventChannel(registrar.messenger(), "plugin.bughub.dev/flt_umengpush_core/event")
-            eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
-                override fun onListen(p0: Any?, sink: EventChannel.EventSink?) {
-                    // 把eventSink存起来
-                    corePlugin?.eventSink?.setDelegate(sink)
-                }
-
-                override fun onCancel(p0: Any?) {
-                    corePlugin?.eventSink?.setDelegate(null)
-                }
-
-            })
-        }
-
-        @JvmField
-        var pushAgent: PushAgent? = null
-
-        @JvmField
-        var deviceToken: String? = null
-
-        @JvmField
-        var error: String? = null
-
-        @JvmStatic
-        fun register(context: Context) {
-            //获取消息推送代理
-            val pushAgent = PushAgent.getInstance(context)
-            FltUmengpushCorePlugin.pushAgent = pushAgent
-            Log.i("==========","pushAgent:$pushAgent")
-
-            pushAgent.register(object : IUmengRegisterCallback {
-                override fun onSuccess(deviceToken: String?) {
-                    FltUmengpushCorePlugin.deviceToken = deviceToken
-                    Log.i("==========","deviceToken:$deviceToken")
-                }
-
-                override fun onFailure(p0: String?, p1: String?) {
-                    error = p0 + p1
-                    Log.i("==========","error:$error")
-                }
-
-            })
-
-            //自定义通知栏打开动作
-            pushAgent.setNotificationClickHandler { _, uMessage ->
-
-                val eventResult = HashMap<String, Any>()
-                eventResult["event"] = "notificationHandler"
-                eventResult["data"] = uMessage.custom
-
-                corePlugin?.eventSink?.success(eventResult)
-            }
-
-        }
-    }
+    private var flutterPluginBinding: FlutterPlugin.FlutterPluginBinding? = null
+    var pushAgent: PushAgent? = null
+    var deviceToken: String? = null
+    var error: String? = null
+    var activity: Activity? = null
 
 
     override fun onMethodCall(call: MethodCall, result: Result) {
-        when {
-            call.method == "configure" -> {
+        when (call.method) {
+            "configure" -> {
 
-                if (pushAgent==null||deviceToken==null){
-                    eventSink.error("error","配置可能有错，请检查.($error)","")
+                if (pushAgent == null || deviceToken == null) {
+                    eventSink.error("error", "配置可能有错，请检查.($error)", "")
                 }
 
                 deviceToken?.let {
@@ -103,26 +44,12 @@ class FltUmengpushCorePlugin(var registrar: Registrar) : MethodCallHandler {
 
                     eventSink.success(eventResult)
                 }
-
-                //自定义通知栏打开动作
-                //pushAgent?.setNotificationClickHandler { _, uMessage ->
-
-                //    val eventResult = HashMap<String, Any>()
-                //    eventResult["event"] = "notificationHandler"
-                //    eventResult["data"] = uMessage.custom
-
-                //    eventSink.success(eventResult)
-                //}
-
                 result.success(null)
             }
             //添加标签 示例：将“标签1”、“标签2”绑定至该设备
-            call.method == "addTags" -> {
+            "addTags" -> {
 
-                val  tags = call.argument<List<String>>("tags") ?: arrayListOf()
-
-
-
+                val tags = call.argument<List<String>>("tags") ?: arrayListOf()
                 pushAgent?.tagManager?.addTags(TagManager.TCallBack { isSuccess, result ->
 
                 }, *tags.toTypedArray())
@@ -130,9 +57,9 @@ class FltUmengpushCorePlugin(var registrar: Registrar) : MethodCallHandler {
                 result.success(null)
             }
             //删除标签,将之前添加的标签中的一个或多个删除
-            call.method == "deleteTags" -> {
+            "deleteTags" -> {
 
-                val  tags = call.argument<List<String>>("tags") ?: arrayListOf()
+                val tags = call.argument<List<String>>("tags") ?: arrayListOf()
 
                 pushAgent?.tagManager?.deleteTags(TagManager.TCallBack { isSuccess, result ->
 
@@ -141,59 +68,142 @@ class FltUmengpushCorePlugin(var registrar: Registrar) : MethodCallHandler {
                 result.success(null)
             }
             //别名增加，将某一类型的别名ID绑定至某设备，老的绑定设备信息还在，别名ID和device_token是一对多的映射关系
-            call.method == "addAlias" -> {
+            "addAlias" -> {
 
-                val  alias = call.argument<String>("alias")
+                val alias = call.argument<String>("alias")
                 val type = call.argument<String>("type")
 
-                pushAgent?.addAlias(alias,type) { isSuccess, message ->
-                    try{
+                pushAgent?.addAlias(alias, type) { isSuccess, message ->
+                    try {
                         val eventResult = HashMap<String, Any>()
                         eventResult["isSuccess"] = isSuccess
                         eventResult["message"] = message
-                        registrar.activity().runOnUiThread { MethodChannel(registrar.messenger(),"plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_addAlias").invokeMethod("callback",eventResult)}
-                    }catch (e:Exception){}
+                        activity?.runOnUiThread {
+                            MethodChannel(
+                                flutterPluginBinding?.binaryMessenger,
+                                "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_addAlias"
+                            ).invokeMethod("callback", eventResult)
+                        }
+                    } catch (e: Exception) {
+                    }
                 }
 
                 result.success(null)
             }
             //别名绑定，将某一类型的别名ID绑定至某设备，老的绑定设备信息被覆盖，别名ID和deviceToken是一对一的映射关系
-            call.method == "setAlias" -> {
+            "setAlias" -> {
 
-                val  alias = call.argument<String>("alias")
+                val alias = call.argument<String>("alias")
                 val type = call.argument<String>("type")
 
-                pushAgent?.setAlias(alias,type) { isSuccess, message ->
+                pushAgent?.setAlias(alias, type) { isSuccess, message ->
                     try {
                         val eventResult = HashMap<String, Any>()
                         eventResult["isSuccess"] = isSuccess
                         eventResult["message"] = message
-                        registrar.activity().runOnUiThread { MethodChannel(registrar.messenger(),"plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_setAlias").invokeMethod("callback",eventResult) }
-                    }catch (e:Exception){
-                        Log.i("----",e.message)
+                        activity?.runOnUiThread {
+                            MethodChannel(
+                                flutterPluginBinding?.binaryMessenger,
+                                "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_setAlias"
+                            ).invokeMethod("callback", eventResult)
+                        }
+                    } catch (e: Exception) {
+                        Log.i("----", e.message)
                     }
                 }
                 result.success(null)
             }
             //移除别名ID
-            call.method == "deleteAlias" -> {
+            "deleteAlias" -> {
 
-                val  alias = call.argument<String>("alias")
+                val alias = call.argument<String>("alias")
                 val type = call.argument<String>("type")
 
-                pushAgent?.deleteAlias(alias,type) { isSuccess, message ->
-                    try{
-                    val eventResult = HashMap<String, Any>()
-                    eventResult["isSuccess"] = isSuccess
-                    eventResult["message"] = message
-                    registrar.activity().runOnUiThread { MethodChannel(registrar.messenger(),"plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_deleteAlias").invokeMethod("callback",eventResult) }
-                    }catch (e:Exception){
-                        Log.i("----",e.message)
+                pushAgent?.deleteAlias(alias, type) { isSuccess, message ->
+                    try {
+                        val eventResult = HashMap<String, Any>()
+                        eventResult["isSuccess"] = isSuccess
+                        eventResult["message"] = message
+                        activity?.runOnUiThread {
+                            MethodChannel(
+                                flutterPluginBinding?.binaryMessenger,
+                                "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_deleteAlias"
+                            ).invokeMethod("callback", eventResult)
+                        }
+                    } catch (e: Exception) {
+                        Log.i("----", e.message)
                     }
                 }
                 result.success(null)
             }
             else -> result.notImplemented()
         }
+    }
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        flutterPluginBinding = binding
+
+        //获取消息推送代理
+        pushAgent = PushAgent.getInstance(flutterPluginBinding?.applicationContext)
+        Log.i("==========", "pushAgent:$pushAgent")
+
+        pushAgent?.register(object : IUmengRegisterCallback {
+            override fun onSuccess(token: String?) {
+                deviceToken = token
+                Log.i("==========", "deviceToken:$deviceToken")
+            }
+
+            override fun onFailure(p0: String?, p1: String?) {
+                error = p0 + p1
+                Log.i("==========", "error:$error")
+            }
+
+        })
+
+        //自定义通知栏打开动作
+        pushAgent?.setNotificationClickHandler { _, uMessage ->
+
+            val eventResult = HashMap<String, Any>()
+            eventResult["event"] = "notificationHandler"
+            eventResult["data"] = uMessage.custom
+
+            eventSink.success(eventResult)
+        }
+
+        val channel = MethodChannel(binding.binaryMessenger, "plugin.bughub.dev/flt_umengpush_core")
+        channel.setMethodCallHandler(this)
+        val eventChannel =
+            EventChannel(binding.binaryMessenger, "plugin.bughub.dev/flt_umengpush_core/event")
+        eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
+            override fun onListen(p0: Any?, sink: EventChannel.EventSink?) {
+                // 把eventSink存起来
+                eventSink.setDelegate(sink)
+            }
+
+            override fun onCancel(p0: Any?) {
+                eventSink.setDelegate(null)
+            }
+
+        })
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        pushAgent = null
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+
+    }
+
+    override fun onDetachedFromActivity() {
+
     }
 }
