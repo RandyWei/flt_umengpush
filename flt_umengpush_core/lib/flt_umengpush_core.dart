11be20f9e8f4492cd5dac213ff3ba0a91ece0330
weizhengheng <weizhengheng@chinahrt.com>
2021-11-26 14:02:41 +0800

    migrate null-safety

--- flt_umengpush_core/lib/flt_umengpush_core.dart
+++ flt_umengpush_core/lib/flt_umengpush_core.dart
@@ -3,7 +3,7 @@ import 'dart:ffi';
 
 import 'package:flutter/services.dart';
 
-typedef TokenCallBack(String token);
+typedef TokenCallBack(String? token);
 typedef NotificationCallback(dynamic pushData);
 
 class FltUmengpushCore {
@@ -13,19 +13,19 @@ class FltUmengpushCore {
   /*
   * 监听通知
   * */
-  static Future<Void> listen(
-      {TokenCallBack tokenCallback,
-      NotificationCallback notificationCallback}) async {
+  static Future<Void?> listen(
+      {TokenCallBack? tokenCallback,
+      NotificationCallback? notificationCallback}) async {
     EventChannel eventChannel =
         const EventChannel("plugin.bughub.dev/flt_umengpush_core/event");
     eventChannel.receiveBroadcastStream().listen((channelData) {
       var event = channelData["event"];
       if (event == "configure") {
         var deviceToken = channelData["deviceToken"];
-        tokenCallback(deviceToken);
+        tokenCallback?.call(deviceToken);
       } else if (event == "notificationHandler") {
         var data = channelData["data"];
-        notificationCallback(data);
+        notificationCallback?.call(data);
       }
     }, onError: (Object obj) {
       print(obj);
@@ -37,12 +37,12 @@ class FltUmengpushCore {
   /*
    * 添加标签 示例：将“标签1”、“标签2”绑定至该设备
    */
-  static Future<Void> addTags(List<String> args, {Function callback}) async {
+  static Future<Void?> addTags(List<String> args, {Function? callback}) async {
     MethodChannel(
             "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_addTags")
         .setMethodCallHandler((call) {
       if (call.method == "") {
-        callback();
+        callback?.call();
       }
       return Future.value(null);
     });
@@ -52,20 +52,20 @@ class FltUmengpushCore {
   /*
    * 删除标签,将之前添加的标签中的一个或多个删除
    */
-  static Future<Void> deleteTags(List<String> args) async {
+  static Future<Void?> deleteTags(List<String> args) async {
     return await _channel.invokeMethod("deleteTags", {"tags": args});
   }
 
   /*
    * 别名增加，将某一类型的别名ID绑定至某设备，老的绑定设备信息还在，别名ID和device_token是一对多的映射关系
    */
-  static Future<Void> addAlias(String args,
-      {String type = "CustomType", Function callback}) async {
+  static Future<Void?> addAlias(String args,
+      {String type = "CustomType", Function? callback}) async {
     MethodChannel(
             "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_addAlias")
         .setMethodCallHandler((call) {
       if (call.method == "callback") {
-        callback(call.arguments);
+        callback?.call(call.arguments);
       }
       return Future.value(null);
     });
@@ -76,13 +76,13 @@ class FltUmengpushCore {
   /*
    * 别名绑定，将某一类型的别名ID绑定至某设备，老的绑定设备信息被覆盖，别名ID和deviceToken是一对一的映射关系
    */
-  static Future<Void> setAlias(String args,
-      {String type = "CustomType", Function callback}) async {
+  static Future<Void?> setAlias(String args,
+      {String type = "CustomType", Function? callback}) async {
     MethodChannel(
             "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_setAlias")
         .setMethodCallHandler((call) {
       if (call.method == "callback") {
-        callback(call.arguments);
+        callback?.call(call.arguments);
       }
       return Future.value(null);
     });
@@ -93,13 +93,13 @@ class FltUmengpushCore {
   /*
    * 移除别名ID
    */
-  static Future<Void> deleteAlias(String args,
-      {String type = "CustomType", Function callback}) async {
+  static Future<Void?> deleteAlias(String args,
+      {String type = "CustomType", Function? callback}) async {
     MethodChannel(
             "plugin.bughub.dev/flt_umengpush_core/UTrack.ICallBack_deleteAlias")
         .setMethodCallHandler((call) {
       if (call.method == "callback") {
-        callback(call.arguments);
+        callback?.call(call.arguments);
       }
       return Future.value(null);
     });
